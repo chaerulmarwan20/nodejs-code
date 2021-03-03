972728e4cb22023fa5589e32962761b330d959d4
chaerulmarwan20 <chaerulmarwan20@gmail.com>
2021-03-03 11:10:28 +0700

    menghapus folder yang tiak terpakai

--- petani-kode/01-pengenalan-dasar-nodejs/server.js
+++
@@ -1,9 +0,0 @@
-let http = require('http');
-
-let server = http.createServer((req, res) => {
-	res.end("Hi, selamat datang di nodejs");
-});
-
-server.listen(8000);
-
-console.log("server running on http://localhost:8000");
